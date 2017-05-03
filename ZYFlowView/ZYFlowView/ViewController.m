e3813fefbcb8ecc6ac3b911845dd61615cd557f6
高志远 <zhiyuan.gzy@alibaba-inc.com>
2017-05-03 17:49:10 +0800

    小修改

--- ZYFlowView/ZYFlowView/ViewController.m
+++ ZYFlowView/ZYFlowView/ViewController.m
@@ -17,7 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSArray *labelArray = @[@"农业银行",@"中国人民银行",@"工商银行",@"中国建生银行",@"感冒病毒发烧",@"咳嗽",@"爱品商城",@"娱乐大咖尼古拉斯",@"iOS行业现状",@"女神和小张的故事",@"地铁分析",@"张柏芝"];
+    NSArray *labelArray = @[@"农业银行",@"中国人民银行",@"工商银行",@"中国建生银行",@"感冒病毒发烧",@"咳嗽",@"爱品商城",@"娱乐大咖尼古拉斯",@"iOS行业现状",@"女神和小张的故事",@"地铁分析",@"张柏芝",@"社会主义精神"];
     ZYFlowView *flowView = [[ZYFlowView alloc] initWithFrame:CGRectMake(0, 100, 375, 300) array:labelArray];
     [self.view addSubview:flowView];
 }
